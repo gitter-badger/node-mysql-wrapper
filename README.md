089c0038f0c41a6fb2bb8ee46f62cdd98e5fe4c9
Makis Maropoulos <makis@ideopod.com>
2015-07-30 18:12:47 +0300

    Update npm version to 1.0.3

--- README.md
+++ README.md
@@ -26,18 +26,28 @@ var mysqlCon =  require('node-mysql-wrapper')(dbConfig.URL);
 
 mysqlCon.connect().then(function () {
     //load models
-  
-    var user = {userId:1 , username: 'a username', password: ' a pass',createdDate : '27/07/2015',noInDatabaseProperty:'something else that must NOT shown as column below!'};
+
+    var user = { userId: 17, mail: 'mail17@omakis.com', username: 'an updated17 username', password: 'a pass', noInDatabaseProperty: 'something else that must NOT shown as column below!' };
     var userTable = mysqlCon.table('users');
-    var userModel = userTable.createModel(user);
-    
-    console.log('Columns of this user: ' + userModel.columns + ' \nValues: ' + userModel.values + ' \ntable name: ' + userModel.table.name);
-});
+    var userModel = userTable.model(user);
 
+    console.log('Columns of this user: ' + userModel.table.columns + ' \nValues: ' + userModel.values + ' \ntable name: ' + userModel.table.name);
 
-var httpPort = config.get('Server.port');
-httpServer.listen(httpPort, function() {
-  console.log("Server is running on " + httpPort);
+    /* console.log("============= try to save==============");
+     userModel.save().then(function (_user) {
+         console.log(user.userId + " from call back is the same object: " + _user.userId);
+     });
+       
+     console.log("================ try to delete ===============");
+     userModel.delete().then(function (_deleted) {
+         console.log("user with username: " + _deleted.username + " just deleted from db");
+         delete user;
+         delete userModel;
+     }, function (err) {
+         console.log(err);
+     });
+      */
+
 });
 
 
--- examples/server.js
+++ examples/server.js
@@ -5,36 +5,38 @@ var path = require('path');
 var config = require('config');
 var dbConfig = require('./config/database.json')[process.env.NODE_ENV || 'development'];
 //FIRST TESTS
-var mysqlCon =  require('./../index')(dbConfig.URL); 
+var mysqlCon = require('./../index')(dbConfig.URL);
 
 
 
 mysqlCon.connect().then(function () {
     //load models
-  
-    var user = {userId:17,mail:'mail17@omakis.com',username: 'an updated17 username', password: 'a pass',noInDatabaseProperty:'something else that must NOT shown as column below!'};
+
+    var user = { userId: 0, mail: 'mail17@omakis.com', username: 'an updated17 username', password: 'a pass', noInDatabaseProperty: 'something else that must NOT shown as column below!' };
     var userTable = mysqlCon.table('users');
     var userModel = userTable.model(user);
-    
+
     console.log('Columns of this user: ' + userModel.table.columns + ' \nValues: ' + userModel.values + ' \ntable name: ' + userModel.table.name);
 
-   /* console.log("============= try to save==============");
-    userModel.save().then(function (_user) {
-        console.log(user.userId + " from call back is the same object: " + _user.userId);
-    });
-      
-    console.log("================ try to delete ===============");
-    userModel.delete().then(function (_deleted) {
-        console.log("user with username: " + _deleted.username + " just deleted from db");
-        delete user;
-        delete userModel;
-    });
- */
+    /* console.log("============= try to save==============");
+     userModel.save().then(function (_user) {
+         console.log(user.userId + " from call back is the same object: " + _user.userId);
+     });
+       
+     console.log("================ try to delete ===============");
+     userModel.delete().then(function (_deleted) {
+         console.log("user with username: " + _deleted.username + " just deleted from db");
+         delete user;
+         delete userModel;
+     }, function (err) {
+         console.log(err);
+     });
+      */
 
 });
 
 
 var httpPort = config.get('Server.port');
-httpServer.listen(httpPort, function() {
-  console.log("Server is running on " + httpPort);
+httpServer.listen(httpPort, function () {
+    console.log("Server is running on " + httpPort);
 });
