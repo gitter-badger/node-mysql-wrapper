fdedf419af96dda81e79ae140af33db4a0dbd086
Makis Maropoulos <makis@ideopod.com>
2015-08-28 05:54:27 +0300

    New definitely_typed works with latest version :)

---
+++ definitely_typed/node-mysql-wrapper/node-mysql-wrapper-tests.ts
@@ -0,0 +1,24 @@
+///<reference path='./node-mysql-wrapper.d.ts' />
+
+var wrapper = require("node-mysql-wrapper");
+var db = wrapper.wrap("mysql://kataras:pass@127.0.0.1/taglub?debug=false&charset=utf8");
+
+db.ready(() => {
+
+    db.table("users").on("insert", (parsedResults) => {
+
+    });
+
+    db.table("users").findAll().then((results) => {
+        console.dir(results);
+    });
+
+    db.table("users").find({ yearsOld: 22 }, (results) => {
+        console.dir(results);
+    });
+
+    db.table("users").findById(18, (result) => {
+        console.dir(result);
+    });
+
+});
